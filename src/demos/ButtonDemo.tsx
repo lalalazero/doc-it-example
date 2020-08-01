49f61331798483212c4c144b7dddf103ef0055c4
lalalazero <zzzero520@hotmail.com>
2020-08-01 17:28:26 +0800

    feat: custom component works fine !

--- src/demos/ButtonDemo.tsx
+++ src/demos/ButtonDemo.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Button } from 'antd'
 import 'antd/dist/antd.css'
+import { Button as CustomButton } from '../index'
 
-const demo = <Button type="primary">button11</Button>
+const demo = <div>
+    <CustomButton>cutom button+111</CustomButton>
+    <Button type="primary">button11</Button>
+</div>
 
 export default demo
